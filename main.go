4f43f39a99a4aa48f6a9de8260a96ad43b77dba9
taishi <39787700+taiseaocean@users.noreply.github.com>
2021-09-05 20:15:35 +0900

    postgres test

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"go-gqlgen/config"
+	pg "go-gqlgen/database"
+	"log"
+)
+
+func main() {
+	ctx := context.Background()
+
+	config := config.New()
+
+	db := pg.New(ctx, config)
+
+	if err := db.Migration(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Server started")
+}
