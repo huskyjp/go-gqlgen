2dd1b1c02d26e219c370ac06a75fc0d08503a373
taishi <39787700+taiseaocean@users.noreply.github.com>
2021-09-07 00:39:13 +0900

    added graphql server

--- main.go
+++ main.go
@@ -5,7 +5,15 @@ import (
 	"fmt"
 	"go-gqlgen/config"
 	pg "go-gqlgen/database"
+	"go-gqlgen/graph/generated"
+	graph "go-gqlgen/graph/resolver"
 	"log"
+	"time"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
@@ -19,5 +27,25 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Server started")
+	// setting router with chi
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RedirectSlashes)
+	r.Use(middleware.Timeout(time.Second * 60))
+
+	r.Handle("/", playground.Handler("GraphQL Golang", "/query"))
+	r.Handle("/query", handler.NewDefaultServer(
+		generated.NewExecutableSchema(
+			generated.Config{
+				Resolvers: &graph.Resolver{},
+			},
+		),
+	))
+
+	fmt.Println("GraphQL Server is About to Running....")
+
 }
