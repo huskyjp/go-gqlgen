ddc948674a1ae393663f6f9e6a61c6d72e3c7e10
taishi <39787700+taiseaocean@users.noreply.github.com>
2021-09-07 11:57:18 +0900

    added registration resolver

--- graph/resolver/auth_resolver.go
+++ graph/resolver/auth_resolver.go
@@ -3,11 +3,40 @@ package graph
 import (
 	"context"
 	"fmt"
+	domain "go-gqlgen/domain/entity"
 	"go-gqlgen/graph/model"
+	"log"
 )
 
-func (r *mutationResolver) Register(ctx context.Context, input model.RegisterInput) (*model.AuthResponse, error) {
-	panic(fmt.Errorf("not implemented"))
+func (r *mutationResolver) Register(ctx context.Context, input model.RegisterInput) (*domain.AuthRegisterResponse, error) {
+	res, err := r.AuthRepository.Register(ctx, domain.AuthRegisterInput{
+		Email:        input.Email,
+		Username:     input.Username,
+		Password:     input.Password,
+		AuthPassword: input.AuthPassword,
+	})
+	if err != nil {
+		log.Fatalln("error happend when registration: %v", err)
+
+	}
+
+	return mapAuthResponse(res), nil
+}
+
+func mapAuthResponse(r domain.AuthRegisterResponse) *domain.AuthRegisterResponse {
+	return &domain.AuthRegisterResponse{
+		AccessToken: r.AccessToken,
+		User:        *mapUser(r.User),
+	}
+}
+
+func mapUser(u domain.User) *domain.User {
+	return &domain.User{
+		ID:        u.ID,
+		Email:     u.Email,
+		Username:  u.Username,
+		CreatedAt: u.CreatedAt,
+	}
 }
 
 func (r *mutationResolver) Login(ctx context.Context, input model.LoginInput) (*model.AuthResponse, error) {
