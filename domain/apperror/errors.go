117b05d27b3ac3ab7fff4e78af544551b4630023
taishi <39787700+taiseaocean@users.noreply.github.com>
2021-09-11 10:48:36 +0900

    added new error message

--- domain/apperror/errors.go
+++ domain/apperror/errors.go
@@ -6,5 +6,6 @@ var (
 	ErrNotFound                 = errors.New("not found")
 	ErrValidation               = errors.New("validation error")
 	ErrUserNameIsTaken          = errors.New("username is already taken")
+	ErrEmailIsTaken             = errors.New("email is already taen")
 	ErrNotFoundUserNamePassword = errors.New("username and password does not match")
 )
